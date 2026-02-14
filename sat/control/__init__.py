092fe3a5cceb1a9b264b3308f333cffe3bbb5c2f
rayanam2021 <rayanam2021@gmail.com>
2026-02-14 11:53:09 -0800

    RTN-to-ECI and propagation to get ECI position/velocity

--- sat/control/__init__.py
+++ sat/control/__init__.py
@@ -1,3 +1,15 @@
 """
 Attitude control and guidance algorithms.
 """
+
+from .rtn_to_eci_propagate import (
+    relative_rtn_to_absolute_eci,
+    propagate_deputy,
+    rv_to_kepler,
+)
+
+__all__ = [
+    "relative_rtn_to_absolute_eci",
+    "propagate_deputy",
+    "rv_to_kepler",
+]
